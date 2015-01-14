416c1462ea2d90dcf42cc85de9238825e6b66e2f
AlexKomrakov <a.komrakov@smart-crowd.ru>
2015-01-14 03:02:22 +0300

    + randomInt with parameters

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,8 @@ import (
 	"reflect"
 	"time"
 	"regexp"
+	"strings"
+	"strconv"
 )
 
 func randomBool() bool {
@@ -12,21 +14,36 @@ func randomBool() bool {
 	return rand.Intn(2) == 0
 }
 
-func randomInt() int {
+func randomInt(max interface {}) int {
+	string, _ := max.(string) //TODO обработка ошибок
+	max_int, _ := strconv.ParseInt(string, 10, 0)
+
 	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(100)
+	return rand.Intn(int(max_int))
+}
+
+func getParameters(str string) []reflect.Value {
+	str = strings.Trim(str, "}{% ")
+	params := strings.SplitN(str, ":", 2) //TODO автоматически определять размер параметров
+
+	result := make([]reflect.Value, len(params)-1)
+	for key, value := range params[1:] {
+		result[key] = reflect.ValueOf(value)
+	}
+
+	return result
 }
 
 func ParseString(str string) interface {} {
 	functions := map[string]interface{}{
 		"{% randomBool %}": randomBool,
-		"{% randomInt(:[\\d]+)? %}": randomInt,
+		"{% randomInt:[\\d]+ %}": randomInt,
 	}
 	for pattern, fnc := range functions {
 		matched, _ := regexp.Match(pattern, []byte(str))
 		if matched {
 			f := reflect.ValueOf(fnc)
-			params := make([]reflect.Value,0)
+			params := getParameters(str)
 			return f.Call(params)[0].Interface()
 		}
 	}
